952411a359a06cd2376721d416c75a56935281f3
Don Mennerich <don.mennerich@nyu.edu>
2020-02-21 11:32:02 -0500

    Writing config to variable

--- Auth.go
+++ Auth.go
@@ -10,17 +10,24 @@ type Configuration struct {
 	password string
 }
 
+var conf Configuration
+
 func Init() error {
 	viper.SetConfigFile("conf.json")
 	viper.AddConfigPath("github.com/nyudlts/go-aspace")
-	var configuration Configuration
 
 	if err := viper.ReadInConfig(); err != nil {
 		return err
 	}
 
-	err := viper.Unmarshal(&configuration)
+	if err := viper.Unmarshal(&conf); err != nil {
+		return err
+	}
 
-	return err
+	conf.username = viper.GetString("username")
+	conf.password = viper.GetString("password")
+	conf.url = viper.GetString("url")
+
+	return nil
 
 }
