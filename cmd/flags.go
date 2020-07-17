d67401f4ced99900c8f63d41dc14f348ae5ed5b2
Don Mennerich <don.mennerich@nyu.edu>
2020-07-17 09:07:49 -0400

    git hell

--- cmd/flags.go
+++ cmd/flags.go
@@ -3,19 +3,20 @@ package cmd
 import "github.com/nyudlts/go-aspace/lib"
 
 var (
-	resourceId   int
-	repositoryId int
-	location     string
-	daos         bool
-	unpub        bool
-	num_cs       bool
-	ead3         bool
-	pdf          bool
-	client       lib.ASClient
-	validate     bool
-	size         int
-	repositories string
-	filename     string
-	published    bool
-	input        string
+	resourceId   	int
+	repositoryId 	int
+	location     	string
+	daos			bool
+	unpub			bool
+	num_cs			bool
+	ead3			bool
+	pdf				bool
+	client			lib.ASClient
+	validate		bool
+	size			int
+	repositories	string
+	filename		string
+	published		bool
+	input			string
+	uri				string
 )
