a90cb773c81ff95297e41a2a809f00d6004bcb18
dmnyu <don.mennerich@nyu.edu>
2020-11-02 12:06:25 -0500

    updating auth function

--- ASClient_test.go
+++ ASClient_test.go
@@ -1,67 +1,28 @@
 package aspace
 
 import (
-	"regexp"
+	"flag"
 	"testing"
 )
 
-func TestInit(t *testing.T) {
-	err := initConfig()
-	if err != nil {
-		t.Error(err)
-	}
-}
+var envPtr = flag.String("environment", "dev", "The environment to run tests on")
 
-func TestConfValues(t *testing.T) {
-	err := initConfig()
-	if err != nil {
-		t.Error(err)
-	}
-	got := len(conf.username)
-	if got <= 0 {
-		t.Errorf("username string length is %d", got)
-	}
 
-	got = len(conf.password)
-	if got <= 0 {
-		t.Errorf("password string length is 0")
-	}
+func TestASClient(t *testing.T) {
+	flag.Parse()
 
-	got = len(conf.url)
-	if got <= 0 {
-		t.Errorf("url string length is 0")
-	}
 
-	var urlPat = regexp.MustCompile(`^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$`)
-	if !urlPat.MatchString(conf.url) {
-		t.Errorf("URL is malformed")
-	}
-
-}
-
-func TestGetSessionKey(t *testing.T) {
-	err := initConfig()
-	if err != nil {
-		t.Error(err)
-	}
-	sessionKey, err := getSessionKey()
-	if err != nil {
-		t.Error(err)
-	}
-	want := 64
-	got := len(sessionKey)
-	if want != got {
-		t.Errorf("wanted key length of %d, got %d", want, got)
-	}
-
-}
-
-func TestNewClientHasSession(t *testing.T) {
-
-	want := 64
-	got := len(Client.sessionKey)
-	if want != got {
-		t.Errorf("wanted key length of %d, got %d", want, got)
-	}
+	t.Run("Test get session key request", func(t *testing.T) {
+		client, err := NewClient(*envPtr, 10)
+		if err != nil {
+			t.Error(err)
+		}
+		want := 64
+		t.Log(client.sessionKey)
+		got := len(client.sessionKey)
+		if want != got {
+			t.Errorf("wanted key length of %d, got %d", want, got)
+		}
+	})
 
 }
--- README.md
+++ README.md
@@ -3,10 +3,10 @@ a Go library for ArchivesSpace integrations
 
 ## Use
 1. Get the go-aspace library: $go get github.com/nyudlts/go-aspace.
-2. Edit the config file `$GOROOT/github.com/nyudlts/go-aspace/go-aspace_template`, enter aspace credentials
-3. Move config file to `$HOME/go-aspace`, `/etc/go-aspace`, or `/etc/sysconfig/go-aspace`, or leave it in `$GOROOT/src/github.com/nyudlts/go-aspace/go-aspace`.
+2. Edit the config file `$GOROOT/github.com/nyudlts/go-aspace/go-aspace.yml_template`, enter aspace credentials
+3. Move config file to `/etc/go-aspace.yml`.
 4. Import "github.com/nyudlts/go-aspace"
-5. An instance of the aspace client is now accessible at aspace.Client
+5. Create an instance of the aspace client:
 
 ```go
 package main
@@ -17,11 +17,16 @@ import (
 )
 
 func main() {
-	aspaceClient := aspace.Client
-	repositories, err := aspaceClient.GetRepositoryList()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(repositories) //prints the array of Repository IDs from ArchivesSpace.
+    aspaceClient, err := aspace.NewClient("environment", timeout)
+    if err != nil {
+        panic(err)
+    }
+    
+    repositories, err := aspaceClient.GetRepositoryList()
+    if err != nil {
+        panic(err)
+    }
+	
+    fmt.Println(repositories) //prints the array of Repository IDs from ArchivesSpace.
 }
 ```
