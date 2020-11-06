e48147338dd94a9210f2639cb7341c2fe4f77f5c
dmnyu <don.mennerich@nyu.edu>
2020-11-06 11:49:56 -0500

    setting up functions

--- DigitalObjects.go
+++ DigitalObjects.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"net/http"
+	"strings"
 )
 
 func (a *ASClient) GetDigitalObjectsByRepositoryId(repositoryId int) ([]int, error) {
@@ -90,4 +92,12 @@ func (a *ASClient) DeleteDigitalObject(repositoryId int, doId int) (string, erro
 		return string(body), err
 	}
 	return string(body), nil
+}
+
+func (a *ASClient) GetDigitalObjectsIDsForResource(repositoryId int, resourceId int) ([]int, error) {
+	resourceTree, err := a.GetResourceTree(repositoryId, resourceId)
+}
+
+func (a *ASClient) GetDigitalObjectsForResource(repositoryId int, resourceId int) ([]DigitalObject, error) {
+
 }
