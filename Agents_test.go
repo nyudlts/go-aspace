60f96063c1eb3a9b2a99b2aafa649ac72c3d987f
Don Mennerich <don.mennerich@nyu.edu>
2020-12-08 08:32:28 -0500

    Updating Agent tests

--- Agents_test.go
+++ Agents_test.go
@@ -12,16 +12,48 @@ func TestPersonalAgent(t *testing.T) {
 		t.Error(err)
 	}
 
-	agentId, err := client.GetRandomAgentID("people")
-	if err != nil {
-		t.Error(err)
-	}
+	t.Run("Test a person agent", func(t *testing.T) {
+		agentId, err := client.GetRandomAgentID("people")
+		if err != nil {
+			t.Error(err)
+		}
 
-	agent, err := client.GetAgent("people", agentId)
-	if err != nil {
-		t.Error(err)
-	} else {
-		t.Logf("Successfully requested and serialized agent %s: %s\n", agent.URI, agent.Names[0].SortName)
-	}
+		agent, err := client.GetAgent("people", agentId)
+		if err != nil {
+			t.Error(err)
+		} else {
+			t.Logf("Successfully requested and serialized person agent %s: %s\n", agent.URI, agent.Names[0].SortName)
+		}
 
+	})
+
+	t.Run("Test a family agent", func(t *testing.T) {
+		agentId, err := client.GetRandomAgentID("families")
+		if err != nil {
+			t.Error(err)
+		}
+
+		agent, err := client.GetAgent("families", agentId)
+		if err != nil {
+			t.Error(err)
+		} else {
+			t.Logf("Successfully requested and serialized family agent %s: %s\n", agent.URI, agent.Names[0].SortName)
+		}
+
+	})
+
+	t.Run("Test a corporate entitiy agent", func(t *testing.T) {
+		agentId, err := client.GetRandomAgentID("corporate_entities")
+		if err != nil {
+			t.Error(err)
+		}
+
+		agent, err := client.GetAgent("corporate_entities", agentId)
+		if err != nil {
+			t.Error(err)
+		} else {
+			t.Logf("Successfully requested and serialized corporate entity agent %s: %s\n", agent.URI, agent.Names[0].SortName)
+		}
+
+	})
 }
