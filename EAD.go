32cd93072f3962bd02104d6207130025bafc3eb8
Don Mennerich <don.mennerich@nyu.edu>
2024-02-06 13:35:55 -0500

    Re-adding EAD.go

---
+++ EAD.go
@@ -0,0 +1,36 @@
+package aspace
+
+import (
+	"fmt"
+	"io"
+)
+
+func (a *ASClient) GetEADAsByteArray(repositoryId int, resourceId int, unpublished bool) ([]byte, error) {
+	eadBytes := []byte{}
+	endpoint := fmt.Sprintf("/repositories/%d/resource_descriptions/%d.xml?include_unpublished=%t&include_daos=%t&numbered_cs=%t&ead3=%t&print_pdf=%t", repositoryId, resourceId, unpublished, true, false, false, false)
+	response, err := a.get(endpoint, true)
+	if err != nil {
+		return eadBytes, err
+	}
+
+	eadBytes, err = io.ReadAll(response.Body)
+	return eadBytes, err
+}
+
+func (a *ASClient) SerializeEAD(repositoryId int, resourceId int, daos bool, unpub bool, num_cs bool, ead3 bool, pdf bool) ([]byte, error) {
+	var ead []byte = []byte{}
+
+	endpoint := fmt.Sprintf("/repositories/%d/resource_descriptions/%d.xml?include_unpublished=%t&include_daos=%t&numbered_cs=%t&ead3=%t&print_pdf=%t", repositoryId, resourceId, unpub, daos, num_cs, ead3, pdf)
+	response, err := a.get(endpoint, true)
+	if err != nil {
+		return ead, err
+	}
+
+	ead, err = io.ReadAll(response.Body)
+	if err != nil {
+		return ead, err
+	}
+
+	return ead, nil
+
+}
