bd391dbdf0c43dda8d8673c3fcec3921372aef55
Donald Mennerich <don.mennerich@nyu.edu>
2025-09-11 14:47:22 -0400

    adding random string generator

--- Common.go
+++ Common.go
@@ -158,3 +158,13 @@ func ParseCreateOrUpdateResponse(body string) *CreateOrUpdateResponse {
 	}
 	return &cour
 }
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
